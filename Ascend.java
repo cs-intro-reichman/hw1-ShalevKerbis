8c72e802a8bc85e18cf4dc9ab43ba532cc4db947
שלו קרביס <shalev@shalevs-MacBook-Pro.local>
2024-11-07 18:01:39 +0200

    completed question 4

--- Ascend.java
+++ Ascend.java
@@ -4,16 +4,17 @@ public class Ascend {
 	public static void main(String[] args) {
 		int limit = Integer.parseInt(args[0]);
 
+		/* multyplying the random numbers by the limit in order to
+		   expand the range of the random numbers up to the limit. */
 		int a = (int) (Math.random() * limit);
 		int b = (int) (Math.random() * limit);
 		int c = (int) (Math.random() * limit);
 
-		System.out.println(a + " " + b + " " + c + " ");
-
 		int smallest = Math.min(c, Math.min(a, b));
 		int largest = Math.max(c, Math.max(a, b));
-		int middle = a + b + c - smallest - largest;
+		int middle = a + b + c - (smallest + largest);
 
+		System.out.println(a + " " + b + " " + c + " ");
 		System.out.println(smallest + " " + middle + " " + largest);
 	}
 }

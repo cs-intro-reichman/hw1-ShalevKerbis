8dcdb297c5027eafcd916bd7b7fa097ada1db49d
שלו קרביס <shalev@shalevs-MacBook-Pro.local>
2024-11-07 17:47:54 +0200

    completed question 5

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,7 +1,6 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) { 
-		String time = args[0];
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 
